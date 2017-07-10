12e2d9eff7937656ed5fcc36dd6be3e35edfda8b
Yang Lorenzana <Yang Lorenzana>
2017-07-10 18:39:26 -0400

    Added /todos POST endpoint

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose.js');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+  
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+  console.log(req.body);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port: ' + port);
+})
