cf2ac392aae0a91d2ea5f1af8d181a75d919fccc
Yang Lorenzana <Yang Lorenzana>
2017-07-12 01:45:35 -0400

    Added /todos:id GET endpoint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose.js');
 const { Todo } = require('./models/todo');
@@ -27,10 +28,28 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({
       todos
-    })
+    });
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) { 
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({todo});      
+    }
+    else {
+      res.status(404).send();
+    }
+  }, (e) => {
+    res.status(400).send();
+  });
+  
 });
 
 app.listen(3000, () => {
