337404a2d70c436a525caa22c6fb55dc632c40b4
Yang Lorenzana <Yang Lorenzana>
2017-07-14 23:11:06 -0400

    Added tests for DELETE /todos/:id endpoint

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,40 @@ describe('GET /todos/:id', () => {
       .end(done)
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should remove a todo', (done) => {
+    let id = todos[1]._id.toHexString();
+    request(app)
+      .delete('/todos/' + id)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(id).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('Should return a 404 if todo not found', (done) => {
+    request(app)
+      .delete('/todos/' + new ObjectID())
+      .expect(404)
+      .end(done);
+  });
+
+  it('Should return a 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123a')
+      .expect(404)
+      .end(done);
+  });
+
+});
