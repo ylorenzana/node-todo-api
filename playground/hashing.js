72f5c0d892dbc8faec9409372223021da084e76e
Yang Lorenzana <Yang Lorenzana>
2017-07-25 23:52:47 -0400

    Added password hashing before storing on db

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = 'nikawat';
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  })
+});
 
-let token = jwt.sign(data, 'salt');
-console.log(token);
+let hashedPassword = '$2a$10$f2XBUSbqbhu/IkmiYNSmb.L7qgp38NcOjVXm3hWnmUW8Mya5fvU.G';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-let decoded = jwt.verify(token, 'salt');
-console.log(decoded);
+
+// let data = {
+//   id: 10
+// };
+
+// let token = jwt.sign(data, 'salt');
+// console.log(token);
+
+// let decoded = jwt.verify(token, 'salt');
+// console.log(decoded);
 
 // let message = 'I am user #2';
 // let hash = SHA256(message).toString();
